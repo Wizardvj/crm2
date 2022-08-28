2528124740a584008df77101b578be5190bd47b5
shettyvijeth99@gmail.com <shettyvijeth99@gmail.com>
2022-08-29 00:15:11 +0530

    crm5

---
+++ src/main/java/com/example/ProjCrm5Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjCrm5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjCrm5Application.class, args);
+	}
+
+}
